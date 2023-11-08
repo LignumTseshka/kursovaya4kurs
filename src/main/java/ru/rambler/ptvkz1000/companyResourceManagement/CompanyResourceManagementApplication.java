ea573129af67687392af14347e9560ac2c0f40b0
Andrei <ptvkz1000@rambler.ru>
2023-11-08 20:06:54 +0300

    initial commit

---
+++ src/main/java/ru/rambler/ptvkz1000/companyResourceManagement/CompanyResourceManagementApplication.java
@@ -0,0 +1,13 @@
+package ru.rambler.ptvkz1000.companyResourceManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyResourceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyResourceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rambler/ptvkz1000/companyResourceManagement/CompanyResourceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rambler.ptvkz1000.companyResourceManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyResourceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
